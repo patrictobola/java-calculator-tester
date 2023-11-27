7a1fb13ed0edec1e7ea3be169f2a1d6c346a5748
patrictobola <tobola92@gmail.com>
2023-11-27 12:55:25 +0100

    done calculator

--- src/main/java/org/java/Calculator.java
+++ src/main/java/org/java/Calculator.java
@@ -2,9 +2,27 @@ package org.java;
 
 public class Calculator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static float add(float num1, float num2) {
+		return num1 + num2;
 	}
 
+	public static float subtract(float num1, float num2) {
+		return num1 - num2;
+	}
+
+	public static float divide(float num1, float num2) {
+		if (num2 != 0) {
+			return num1 / num2;
+		} else {
+			System.out.println("Errore: Impossibile dividere per zero.");
+			return Float.NaN; // Not a Number
+		}
+	}
+
+	public static float multiply(float num1, float num2) {
+		return num1 * num2;
+	}
+	public static void main(String[] args) {
+		System.out.println(divide(8,5));
+	}
 }
